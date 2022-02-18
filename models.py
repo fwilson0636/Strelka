b8512efa45d6cbd7e89dbc5f2cca49d97e10586a
Faith Wilson <fwilson@aggies.ncat.edu>
2022-02-18 16:43:23 -0500

    Cleaned up jpeg.py and created models.py for methods of functionality

---
+++ models.py
@@ -0,0 +1,24 @@
+def parse_jpeg_from_path(path: str) -> dict:
+    """
+    This function will take the path of a JPEG image file as input and produce a dictionary from the requested list of metadata fields.
+    :param path:this is the path to the image file used
+    :return: returns a python dictionary that contains metadata
+    """
+
+    return dict()
+
+
+def parse_jpeg_from_data(data: bytes) -> dict:
+    """
+    This function will take the data of a JPEG image file as input and produce a dictionary from the requested list of metadata fields.
+    :param data:this is the data to the image file used
+    :return: returns a python dictionary that contains metadata
+    """
+
+    return dict()
+
+
+#output
+#error handling
+#field validation
+#mock scanner
