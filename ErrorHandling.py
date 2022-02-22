4de2423064b2efcb170a827bc6defa19400228a7
Essence Cain <evcain1@gmail.com>
2022-02-22 18:46:47 -0500

    Implemented new solution for file error handling

--- ErrorHandling.py
+++
@@ -1,8 +0,0 @@
-class Error(Exception):
-    """Base class for other exceptions"""
-    pass
-
-
-class PathError(Error):
-    """Exception error for invalid file path"""
-    print("Invalid file path")
--- strelka/strelka.py
+++ strelka/strelka.py
@@ -1,2 +1,2 @@
 class Scanner:
-    pass
+    tags = []
